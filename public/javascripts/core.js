2cddf2dd0ac26d2298ec0af4bb79a3280295af58
Jordan <Jordan.Simonovski-1@student.uts.edu.au>
2015-03-18 23:30:46 +1100

    got responsive layout working for cards. Added reddit icon to toolbar

--- public/javascripts/core.js
+++ public/javascripts/core.js
@@ -1,4 +1,18 @@
-var redditApp = angular.module('redditApp', ['ngMaterial']);
+var redditApp = angular.module('redditApp', ['ngMaterial'])
+.config(function($mdThemingProvider) {
+  $mdThemingProvider.theme('default')
+    .primaryPalette('pink', {
+      'default': '400', // by default use shade 400 from the pink palette for primary intentions
+      'hue-1': '100', // use shade 100 for the <code>md-hue-1</code> class
+      'hue-2': '600', // use shade 600 for the <code>md-hue-2</code> class
+      'hue-3': 'A100' // use shade A100 for the <code>md-hue-3</code> class
+    })
+    // If you specify less than all of the keys, it will inherit from the
+    // default shades
+    .accentPalette('indigo', {
+      'default': '400' // use shade 200 for default, and keep all other shades the same
+    });
+});
 
 redditApp.controller('mainController', ['$scope','$http', function($scope, $http){
 	$scope.postList = {};
